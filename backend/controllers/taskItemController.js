e4b276361c29cde10178220542369d905b9dce4a
SHUBHAM BATWAL <156819851+shubhambatwal03@users.noreply.github.com>
2026-07-06 13:06:50 +0530

    Implemented get, put, delete routes and controllers

--- backend/controllers/taskItemController.js
+++ backend/controllers/taskItemController.js
@@ -10,3 +10,26 @@ exports.createTaskItem = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+exports.getAllTaskItems = async (req, res) => {
+  try {
+    const taskItems = await TaskItem.find();
+    res.status(200).json(taskItems);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteTaskItem = async (req, res) => {
+  const { id } = req.params;
+  await TaskItem.findByIdAndDelete(id);
+  res.status(204).json({ _id: id });
+};
+
+exports.markCompleted = async (req, res) => {
+  const { id } = req.params;
+  const taskItem = await TaskItem.findById(id);
+  taskItem.completed = true;
+  await taskItem.save();
+  res.json(taskItem);
+};
--- backend/routes/taskItemRouter.js
+++ backend/routes/taskItemRouter.js
@@ -2,6 +2,9 @@ const express = require("express");
 const taskItemRouter = express.Router();
 const taskItemController = require("../controllers/taskItemController");
 
+taskItemRouter.get("/", taskItemController.getAllTaskItems);
 taskItemRouter.post("/", taskItemController.createTaskItem);
+taskItemRouter.delete("/:id", taskItemController.deleteTaskItem);
+taskItemRouter.put("/:id/completed", taskItemController.markCompleted);
 
 module.exports = taskItemRouter;
